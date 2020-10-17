1dc2807c133b13f3d87200557320cede431efe8a
Kiki <Kiki@Kiki-PC>
2020-10-17 22:26:10 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan4/kambing/PBOIF210119052Latihan4Kambing.java
@@ -0,0 +1,25 @@
+package pboif2.pkg10119052.latihan4.kambing;
+
+/**
+ *
+ * Nama : Rizki Dwi Nugraha
+ * Kelas : IF-2
+ * NIM : 10119052
+ * 
+ */
+public class PBOIF210119052Latihan4Kambing {
+
+    public void tambahKambing(){
+        //Deklarasi Variabel Lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        PBOIF210119052Latihan4Kambing kambingJantan = new PBOIF210119052Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
